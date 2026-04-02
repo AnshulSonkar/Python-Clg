31d48c6a72a22d39ff920c95727d974cb1ce33ea
AnshulSonkar98 <anshulsonkar2222@gamail.com>
2026-04-03 00:35:48 +0530

    initial

---
+++ demo.py
@@ -0,0 +1,31 @@
+# print("hello world")
+
+
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+
+# sum = a + b
+
+# print("Sum =", sum)
+
+
+# num = int(input("Enter a number: "))
+
+# if num % 2 == 0:
+#     print("Even Number")
+# else:
+#     print("Odd Number")
+
+
+a = 5
+b = 10
+
+temp = a
+a = b
+b = temp
+
+print("a =", a)
+print("b =", b)
+
+
+
