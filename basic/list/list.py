1fcee6fb5b94a48c637d4d3835682461b8771125
AnshulSonkar98 <anshulsonkar2222@gamail.com>
2026-04-09 22:43:59 +0530

    initial

---
+++ basic/list/list.py
@@ -0,0 +1,30 @@
+# list properties
+ # 1.mutable
+ # 2.Dynamic in size
+ # 3. mixed data type
+
+# list=[1,2,3,"hello",5,7]
+# # print(list)
+# for i in range(len(list)):
+#     print(list[i],end=" ")
+
+
+# # to create empty list 
+# list=[]
+# list1=list()
+# print(list)
+# print(list1)
+
+
+# Creating a list with different data types
+my_list = ["apple", 42, 3.14, True]
+
+# Accessing items 
+print(my_list[0])  
+print(my_list[-1])
+
+# Slicing 
+print(my_list[1:3]) # Output: [42, 3.14]
+
+
+
