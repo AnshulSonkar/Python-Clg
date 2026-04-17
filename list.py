0c231e597c97ad6d1d4bee4bce5f65c2da46f389
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-04-17 21:28:11 +0530

    Add files via upload

--- list.py
+++ list.py
@@ -49,24 +49,112 @@
 #         print(lst)
 
 
-lst=[[3,2,1,8],[50,3,7,9],[2,3,7,8]]
-# 
-ans=[]
-for i in range(len(lst)):
-    temp=lst[i].copy()
-    if i%2==0:
-        for j in range(len(temp)):
-            if temp[j]%2!=0:
-                temp[j]+=1
-                ans.append(temp)
+# lst=[[3,2,1,8],[50,3,7,9],[2,3,7,8]]
+# # 
+# ans=[]
+# for i in range(len(lst)):
+#     temp=lst[i].copy()
+#     if i%2==0:
+#         for j in range(len(temp)):
+#             if temp[j]%2!=0:
+#                 temp[j]+=1
+#                 ans.append(temp)
 
-            else:
-                for j in range(len(lst)):
-                    if temp[j]%2!=0:
-                        temp[j]+=1
-                        ans.append(temp)
-                        print(lst)
-                        print(ans)
+#             else:
+#                 for j in range(len(lst)):
+#                     if temp[j]%2!=0:
+#                         temp[j]+=1
+#                         ans.append(temp)
+#                         print(lst)
+#                         print(ans)
+
+
+# Delete
+# lst=[1,3,5,12,14,15,23]
+
+# del lst [4]
+# print(lst)
+# lst.pop(3)
+# print(lst)
+
+# lst=[1,3,5,12,14,15,23]
+
+# x=lst.pop(2)
+# print(x)
+# print(lst) 
+
+
+# lst = [10, 20, 30]
+
+# x = lst.index(30)
+# print(x)
+
+
+# lst1 = [1, 2]
+# lst2 = [3, 4]
+
+# lst1.extend(lst2)
+# print(lst1)
+
+
+# lst = [1, 2, 3]
+
+# new_lst = lst.copy()
+# print(new_lst)
+
+
+# lst = [1, 2, 3, 4, 5]
+
+# print(lst[1:4])
+# print(lst[::-1])
+
+
+# lst = [5, 2, 9, 1]
+
+# print(max(lst))
+# print(min(lst))
+
+
+# lst = [1, 2, 3]
+
+# print(2 in lst)
+# print(5 in lst)
+
+
+# lst = [1, 2, 3]
+
+# for i in lst:
+#     print(i)
+
+
+# lst = [1, 2, 3, 4]
+
+# new_lst = [x*x for x in lst]
+# print(new_lst)
+
+
+lst = [1, 2, 3, 4, 5, 6]
+
+while lst:
+    lst.pop()
+    
+print(lst)
+
+# lst = [1, 2, 3]
+
+# while lst:
+#     lst.pop()
+    
+# print(lst)
+
+
+
+
+        
+
+
+
+            
 
 
 
