fcdb4bf2fca0a0a4bd50b42e9cc194680db08d57
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-08-02 11:44:48 +0530

    commit

---
+++ Functions/Functions.py
@@ -0,0 +1,4 @@
+# De
+# What is a Function?
+# A function is a block of reusable code that performs a specific task.
+# Instead of writing the same code again and again, you write it once and call it whenever needed.
