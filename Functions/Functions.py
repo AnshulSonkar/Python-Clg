bd1c00ea87b98e80912acc6781706724f4da7005
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-09-10 23:37:53 +0530

    commit

--- Functions/Functions.py
+++ Functions/Functions.py
@@ -2462,6 +2462,14 @@
 # 
 # 12 .
 # Iterator
+# numbers = [10, 20, 30, 40]
+
+# iterator = iter(numbers)
+
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
 
 
 
