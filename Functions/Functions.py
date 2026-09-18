9fdcc7373bf0a90e4f3f37f2c1a7b6b6d19be099
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-09-18 23:07:45 +0530

    commit

--- Functions/Functions.py
+++ Functions/Functions.py
@@ -3110,6 +3110,18 @@
 # print(add_ten(20))
 # 
 # 
+# 8 .
+# reduce() revision
+# from functools import reduce
+
+
+# numbers = [1, 2, 3, 4, 5]
+
+# result = reduce(lambda a, b: a + b, numbers)
+
+# print(result)
+# 
+# 
 
 
 
