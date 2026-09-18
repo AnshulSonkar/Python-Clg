5c27c93f9d19f977d1958348183e35dafaae7fae
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-09-18 23:11:40 +0530

    commit

--- Functions/Functions.py
+++ Functions/Functions.py
@@ -3132,6 +3132,23 @@
 # print(fibonacci(10))
 # 
 # 
+# 10 .
+# lru_cache solution
+# from functools import lru_cache
+
+
+# @lru_cache
+# def fibonacci(n):
+
+#     if n <= 1:
+#         return n
+
+#     return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+# print(fibonacci(10))
+# 
+# 
 
 
 
